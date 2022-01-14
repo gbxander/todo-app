35a515fe24a67744d89b0ea05ac1ca1b099f6a24
gbxander <gbxander14@gmail.com>
2022-01-13 19:32:56 -0800

    misc: create firebase setup file

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKETS,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+
+class Firebase {
+    constructor() {
+        initializeApp(firebaseConfig)
+        this.db = getFirestore()
+    }
+}
+
+export default Firebase
+
+
+
